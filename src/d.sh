bc717488c43b948b79643eb14388b696d3ce898f
kafkafield <451824293@qq.com>
2016-10-19 13:26:19 +0800

    done

---
+++ src/d.sh
@@ -0,0 +1,48 @@
+echo "10^9" >> a.txt
+java -Xmx32000M homework3.Allsort 1000000000 1 >> a.txt
+java -Xmx32000M homework3.Allsort 1000000000 2 >> a.txt
+echo "2*10^8" >> a.txt
+java -Xmx32000M homework3.Allsort 200000000 1 >> a.txt
+java -Xmx32000M homework3.Allsort 200000000 2 >> a.txt
+java -Xmx32000M homework3.Allsort 200000000 3 >> a.txt
+java -Xmx32000M homework3.Allsort 200000000 4 >> a.txt
+echo "10^8" >> a.txt
+java -Xmx32000M homework3.Allsort 100000000 1 >> a.txt
+java -Xmx32000M homework3.Allsort 100000000 2 >> a.txt
+java -Xmx32000M homework3.Allsort 100000000 3 >> a.txt
+java -Xmx32000M homework3.Allsort 100000000 4 >> a.txt
+echo "10^7" >> a.txt
+java -Xmx32000M homework3.Allsort 10000000 1 >> a.txt
+java -Xmx32000M homework3.Allsort 10000000 2 >> a.txt
+java -Xmx32000M homework3.Allsort 10000000 3 >> a.txt
+java -Xmx32000M homework3.Allsort 10000000 4 >> a.txt
+echo "10^6" >> a.txt
+java -Xmx32000M homework3.Allsort 1000000 1 >> a.txt
+java -Xmx32000M homework3.Allsort 1000000 2 >> a.txt
+java -Xmx32000M homework3.Allsort 1000000 3 >> a.txt
+java -Xmx32000M homework3.Allsort 1000000 4 >> a.txt
+echo "10^5" >> a.txt
+java -Xmx32000M homework3.Allsort 100000 1 >> a.txt
+java -Xmx32000M homework3.Allsort 100000 2 >> a.txt
+java -Xmx32000M homework3.Allsort 100000 3 >> a.txt
+java -Xmx32000M homework3.Allsort 100000 4 >> a.txt
+echo "10^4" >> a.txt
+java -Xmx32000M homework3.Allsort 10000 1 >> a.txt
+java -Xmx32000M homework3.Allsort 10000 2 >> a.txt
+java -Xmx32000M homework3.Allsort 10000 3 >> a.txt
+java -Xmx32000M homework3.Allsort 10000 4 >> a.txt
+echo "10^3" >> a.txt
+java -Xmx32000M homework3.Allsort 1000 1 >> a.txt
+java -Xmx32000M homework3.Allsort 1000 2 >> a.txt
+java -Xmx32000M homework3.Allsort 1000 3 >> a.txt
+java -Xmx32000M homework3.Allsort 1000 4 >> a.txt
+echo "10^2" >> a.txt
+java -Xmx32000M homework3.Allsort 100 1 >> a.txt
+java -Xmx32000M homework3.Allsort 100 2 >> a.txt
+java -Xmx32000M homework3.Allsort 100 3 >> a.txt
+java -Xmx32000M homework3.Allsort 100 4 >> a.txt
+echo "10^1" >> a.txt
+java -Xmx32000M homework3.Allsort 10 1 >> a.txt
+java -Xmx32000M homework3.Allsort 10 2 >> a.txt
+java -Xmx32000M homework3.Allsort 10 3 >> a.txt
+java -Xmx32000M homework3.Allsort 10 4 >> a.txt
